cfe07385dba4b44e5ebec1552c6bc50aaee62e50
yoyo <yoyomm99@gmail.com>
2024-03-28 17:29:40 +0800

    test merge

--- djangonote/djangonote/urls.py
+++ djangonote/djangonote/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("/", TemplateView.as_view(template_name="abc")),
     path("admin/", admin.site.urls),
     path("note/", include("note.urls")),
     path("account/", include("account.urls")),
